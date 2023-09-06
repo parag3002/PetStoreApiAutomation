49b5cfdadf72a718e59c7c5c338ad9b77c2aa063
Parag <debu05101987@gmail.com>
2023-09-07 00:24:35 +0530

    first

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,24 @@
+package api.endpoints;
+
+public class Routes 
+{
+	
+	public static String base_url = "https://petstore.swagger.io/v2";
+	
+	//user model 
+	
+	public static String post_url = base_url+"/user";
+	public static String get_url = base_url+"/user/{username}";
+	public static String update_url = base_url+"/user/{username}";
+	public static String delete_url = base_url+"/user/{username}";
+	
+	
+	//store model
+	
+		// Here will create store model urls
+	
+	// pet model
+		
+		//Here will create pet model urls
+	
+}
